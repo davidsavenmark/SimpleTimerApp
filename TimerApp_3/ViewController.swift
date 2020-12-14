7fca7c7b78e92ef4b6b5fe3b11e2eda411bf10e7
David <dsinemark@gmail.com>
2020-12-15 00:07:04 +0100

    Initial Commit

---
+++ TimerApp_3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimerApp_3
+//
+//  Created by David Sävenmark on 2020-12-15.
+//  Copyright © 2020 David Sävenmark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
